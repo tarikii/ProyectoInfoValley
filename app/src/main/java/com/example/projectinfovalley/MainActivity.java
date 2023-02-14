9c647216d5c1e097327c73245f17a3237d0c06da
tariki <aabouchazougarthtarik@elpuig.xeill.net>
2023-02-14 20:34:22 +0100

    Voy a sufrir esta noche

--- app/src/main/java/com/example/projectinfovalley/MainActivity.java
+++ app/src/main/java/com/example/projectinfovalley/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_menu_general);
+        setContentView(R.layout.fragment_acceso);
     }
 }
